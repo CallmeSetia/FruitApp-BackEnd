015e1edacbd70b6f1c830b5ab6f2cbab24c04528
CallmeSetia <dwi.setia.fardhana@gmail.com>
2024-02-15 02:32:31 +0700

    Laratrust Permission

---
+++ app/Util/Permission.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Util;
+
+class Permission
+{
+    const DEFINE_PERMISSIONS = [
+        'superuser' => [
+            'user-superuser' => 'c,r,u,d',
+            'user-admin' => 'c,r,u,d',
+            'product' => 'c,r,u,d',
+            'transaction' => 'c,r,u,d',
+            'stock' => 'c,r,u, d',
+        ],
+        'admin' => [
+            'user-admin' => 'c,r,u,d',
+            'transaction' => 'c,r,u,d',
+            'product' => 'r,u',
+            'stock' => 'r,u',
+        ],
+    ];
+
+    public function getLaratrustPermissions() {
+        return self::DEFINE_PERMISSIONS;
+    }
+
+}
--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -11,27 +11,5 @@ return [
      */
     'truncate_tables' => true,
 
-    'roles_structure' => [
-        'superuser' => [
-            'user-superuser' => 'c,r,u,d',
-            'user-admin' => 'c,r,u,d',
-            'user-license' => 'c,r,u,d',
-            'license-manager' => 'c,r,u,d',
-            'product' => 'c,r,u,d',
-        ],
-        'admin' => [
-            'user-superuser' => 'r,u',
-            'user-admin' => 'c,r,u,d',
-            'user-license' => 'c,r,u,d',
-            'license-manager' => 'c,r,u',
-            'product' => 'r,u',
-        ],
-    ],
-
-    'permissions_map' => [
-        'c' => 'create',
-        'r' => 'read',
-        'u' => 'update',
-        'd' => 'delete',
-    ],
+    'roles_structure' => (new \App\Util\Permission)->getLaratrustPermissions(),
 ];
