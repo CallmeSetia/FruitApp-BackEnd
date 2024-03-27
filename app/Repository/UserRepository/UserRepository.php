0613325520d1d94fc2d4bacde2e43a6562b28cfc
CallmeSetia <dwi.setia.fardhana@gmail.com>
2024-03-27 15:58:23 +0700

    User REPOSITORY add logic If ROLE CUSTOMER

--- app/Repository/UserRepository/UserRepository.php
+++ app/Repository/UserRepository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\UserRepository;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserRepository implements UserRepositoryInterface
 {
@@ -26,10 +27,10 @@ class UserRepository implements UserRepositoryInterface
             $user_admin = $user_admin->where($request->only($onlyColumn));
         }
         if ($request->role == 'admin' || $request->role == 'superuser') {
-            $user_admin = $user_admin->whereHasRole(['admin', 'superuser']);
+            $user_admin = $user_admin->whereHasRole(['admin', 'superuser'])->orWhere($request->only($onlyColumn));
         }
         if ($request->role == 'customer') {
-            $user_admin = $user_admin->whereHasRole(['customer']);
+            $user_admin = $user_admin->whereHasRole(['customer'])->orWhere($request->only($onlyColumn));
         }
         if ($request->include) {
             $relation = array_filter(explode(',', str_replace(" ", "", $request->include)));
@@ -47,6 +48,7 @@ class UserRepository implements UserRepositoryInterface
     {
         $onlyColumn = [
 //            'id', // Kolom id Auto Increment
+            'avatar',
             'name',
             'username',
             'email',
@@ -54,17 +56,44 @@ class UserRepository implements UserRepositoryInterface
         ];
         $data = $request->only($onlyColumn);
         $data['password'] = Hash::make($data['password']); // Hash the password
+        $data['is_customer'] = $request->role == 'customer' ?  1 : 0;
+        $data['avatar'] = $request->filled('avatar') ? $request->avatar : 'https://via.placeholder.com/200x200.png';
 
-        $createdAdmin = User::create($data);
-        $createdAdmin->addRole($request->role);
+        $createdUser = User::create($data);
+        $createdUser->addRole($request->role);
 
-        return $createdAdmin;
+        if ($request->role == 'customer') {
+            $columnCustomer = [
+                'address',
+                'age',
+                'phone',
+                'gender',
+                'nomor_identitas',
+                'foto_identitas',
+                'email',
+                'identitas_type',
+                'keterangan',
+                'limit_pinjaman',
+            ];
+            $dataInputCustomer = $request->only($columnCustomer);
+            $dataInputCustomer['limit_pinjaman'] = 0; // default 0
+            if ($dataInputCustomer->has('foto_identitas')) {
+                $file = $dataInputCustomer->file('foto_identitas');
+                $fileName = time() . '_' . trim($file->getClientOriginalName());
+                $dataInputCustomer['foto_identitas'] = $file->storeAs('user/customer/foto_identitas/', $fileName, 'public');
+            }
+
+            $createdUser->customer()->create($dataInputCustomer);
+        }
+
+        return $createdUser;
     }
 
     public function update($request)
     {
-        $userAdmin = User::findOrFail($request->id);
+        $userUser = User::findOrFail($request->id);
         $onlyColumn = [
+            'avatar',
             'name',
             'username',
             'email',
@@ -79,16 +108,47 @@ class UserRepository implements UserRepositoryInterface
             $data['password'] = Hash::make($data['password']); // Hash the new password
         }
 
-        if (!empty($userRoles = $userAdmin->getRoles())) {
+        if (!empty($userRoles = $userUser->getRoles())) {
             $role = $userRoles[0];
             if ($roleNew && $roleNew !== $role) {
-                $userAdmin->removeRole($role);
-                $userAdmin->addRoles([$roleNew]);
+                $userUser->removeRole($role);
+                $userUser->addRoles([$roleNew]);
             }
         }
-        $userAdmin->update($data);
 
-        return $userAdmin;
+        // if role request is customer
+        if ($request->role == 'customer') {
+            $columnCustomer = [
+                'address',
+                'age',
+                'phone',
+                'gender',
+                'nomor_identitas',
+                'foto_identitas',
+                'email',
+                'identitas_type',
+                'keterangan',
+                'limit_pinjaman',
+            ];
+            $dataInputCustomer = $request->only($columnCustomer);
+            $customer = $userUser->customer;
+
+            if ($dataInputCustomer->has('foto_identitas')) {
+                if ($customer && $customer->foto_identitas && Storage::exists('public/' . $customer->foto_identitas)) {
+                    // Hapus foto_identitas yang ada dari storage sebelum menyimpan yang baru
+                    Storage::delete('public/' . $customer->foto_identitas);
+                }
+                // simpan
+                $file = $dataInputCustomer->file('foto_identitas');
+                $fileName = time() . '_' . trim($file->getClientOriginalName());
+                $dataInputCustomer['foto_identitas'] = $file->storeAs('user/customer/foto_identitas/', $fileName, 'public');
+            }
+
+            $userUser->customer()->update($dataInputCustomer);
+        }
+
+        $userUser->update($data);
+        return $userUser;
     }
 
 
@@ -96,6 +156,14 @@ class UserRepository implements UserRepositoryInterface
     {
         $userAdmin = User::findOrFail($request->id);
         $userAdmin->delete();
+
+        // Hapus foto_identitas pelanggan jika ada
+        if ($userAdmin->is_customer) {
+            $customer = $userAdmin->customer;
+            if ($customer && $customer->foto_identitas && Storage::exists('public/' . $customer->foto_identitas)) {
+                Storage::delete('public/' . $customer->foto_identitas);
+            }
+        }
 
         return $userAdmin;
     }
